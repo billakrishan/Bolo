34e090e861ea713ce30a1b16064a4f18d19595ad
Krishan <billak@sheridancollege.ca>
2018-11-26 14:57:09 -0500

    Adding Message.java

---
+++ app/src/main/java/com/kriations/bolo/Message.java
@@ -0,0 +1,46 @@
+package com.kriations.bolo;
+
+import java.util.Date;
+
+public class Message {
+    private String text;
+    private String user;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    private long time;
+
+    public Message(String text, String user) {
+        this.text = text;
+        this.user = user;
+        this.time = new Date().getTime();
+    }
+
+    public Message(){
+
+    }
+
+
+}
